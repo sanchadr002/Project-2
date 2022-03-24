f00a8af715e219d787c35f9fdaa484cfeaad7b0e
sanchadr002 <sanchadr002@gmail.com>
2022-03-23 17:03:19 -0700

    add code to display search results and create cards in db

--- controllers/cards.js
+++ controllers/cards.js
@@ -6,6 +6,8 @@ const res = require('express/lib/response')
 const Cards = require('../models/cards')
 const apiUrl = process.env.scryfallApiUrl
 const fetch = require('node-fetch')
+const Decks = require('../models/decks')
+const tempDeck = []
 
 // Create router
 const router = express.Router()
@@ -28,18 +30,23 @@ router.use((req, res, next) => {
 
 // index ALL
 router.get('/', (req, res) => {
-	
-		// console.log(results)
-		res.redirect('/cards/search')
+	res.redirect('/cards/search')
 })
 
 
 router.get('/search', (req, res) => {
-	// if (req.body.instant === 'on'){
-	// 	console.log('instant is checked')
-	// }
-	// console.log(req.body.instant = req.body.instant === 'on' ? true : false)
-	res.render('cards/search')
+	res.render('cards/search', { tempDeck })
+})
+
+router.post('/decks/index', (req, res) => {
+	Decks.create({
+		name: req.body.deckName,
+		cards: tempDeck,
+	})
+	.then(deck => {
+		console.log('this was returned from deck create', deck)
+		res.redirect('/decks/index')
+	})
 
 })
 
@@ -102,48 +109,39 @@ router.put('/results', (req, res) => {
 		
 		await cardObjectsList.data.forEach( (card) => {
 			searchResults.push(card)
-			console.log(card.name)
-			// console.log(card.image_uris)
-			// console.log(card.cmc)
-			console.log(card.color_identity)
-			// console.log(card.type_line)
-			// Cards.create({
-			// 	scryfallApiId: cardObjectsList.data.id,
-			// 	name: cardObjectsList.data.name,
-			// 	// imageUrl: cardObjectsList.data.image_uris.small,
-			// 	mv: cardObjectsList.data.cmc,
-			// 	colorIdentity: cardObjectsList.data.color_identity,
-			// 	cardType: cardObjectsList.data.type_line,
-			// 	// owner: User
-			// results.push(card.name)	
-			
-			// })
 		})
 		// console.log(searchResults)
 		res.render('cards/results', { searchResults })
 	})
 })
 
-// create -> POST route that actually calls the db and makes a new document
-router.post('/', (req, res) => {
-	req.body.owner = req.session.userId
-	// reference to decks
-	Cards.create(req.body)
-		// reference to decks
-		.then(cards => {
-			console.log('this was returned from create', cards)
-			res.redirect('/cards/results')
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-
 router.get('/results', (req, res) => {
 	res.render('cards/results')
 })
 
+// create -> POST route that actually calls the db and makes a new document
+router.post('/search', (req, res) => {
+	req.body.owner = req.session.userId
+	
+	Cards.create({
+		scryfallApiId: req.body.scryfallApiId,
+		name: req.body.name,
+		imageUrl: req.body.imageUrl,
+		mv: req.body.mv,
+		colorIdentity: req.body.colorIdentity,
+		cardType: req.body.cardType
+	})
+	.then(card => {
+		console.log('this was returned from create', card)
+		tempDeck.push(card)
+		console.log('this is being pushed to tempDeck', card)
+		console.log('this is tempDeck so far', tempDeck)
+		res.redirect('/cards/search')
+	})
+	.catch(error => {
+		res.redirect(`/error?error=${error}`)
+	})
+})
 
 // // show route
 // router.get('/:id', (req, res) => {
